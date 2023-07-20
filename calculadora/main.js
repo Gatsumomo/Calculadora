e7db4d2732515fee012c3e29b6a6a3a4a0d5ffd4
Ariana <115928524+Gatsumomo@users.noreply.github.com>
2023-07-20 14:13:41 +0200

    Updating methods main.js
    Adding other methods that achieve the same result

--- calculadora/main.js
+++ calculadora/main.js
@@ -81,4 +81,76 @@ document.addEventListener('DOMContentLoaded', () => {
 const display_text = document.querySelector('.display')
 const igualar = document.querySelector('.igualar')
 
+//operaciones
+function sumar(a,b){
+    return a+b
+}
+function restar(a,b){
+    return a-b
+}
+function dividir(a,b){
+    if(b === 0){
+        alert('no se puede dividir entre 0')
+    }else{
+    return a/b
+    }
+}
+function multiplicar(a,b){
+    return a*b
+}
+
+document.addEventListener('DOMContentLoad', () => {
+    var numeros = [1,2,3,4,5,6,7,8,9]
+    var operadores = ['+','/','*','-']
+    var display = document.querySelector('.display')
+    var clickables = document.querySelectorAll('.clickables')
+    const igualar = document.querySelector('.igualar')
+
+    clickables.forEach(clickable => {
+        var valor = clickable.innerText;
+        var operador1 = '';
+        var operador = '';
+        var operador2 = ''; 
+        
+        if(numeros.includes(valor)){
+            operador1 += valor
+        }else{
+            if(operadores.includes(valor)){
+                operador = valor
+            }else{
+                throw error
+            }
+        }
+        
+        if(numeros.includes(valor)){
+            operador2 += valor
+        }
+
+        if(clickable.parentNode.id == 'igualar'){
+            switch(operador){
+                case '+':
+                    var resultado = sumar(operador1,operador2)
+                    display.appendChild(resultado)
+                break;
+                case '-':
+                    var resultado = restar(operador1, operador2)
+                    display.appendChild(resultado)
+                break;
+                case '/':
+                    var resultado = dividir(operador1,operador2)
+                    display.appendChild(resultado)
+                break;
+                default:
+                    var resultado = dividir(operador1,operador2)
+                    display.appendChil(resultado)
+                break;
+            }   
+        }
+
+        //hacer tic-tac-toe JAVASCRIPT
+        //eval -> investigar
+    })
+})
+
+
 
